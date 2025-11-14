594a64376091ac1facea8b2bf7813fcb50e42abc
Louis Rudolf <meticulou@gmail.com>
2025-11-14 02:51:21 -0900

    Add more URLs to API Factory

--- AtomicAssetsApiClient/AtomicAssetsApiFactory.cs
+++ AtomicAssetsApiClient/AtomicAssetsApiFactory.cs
@@ -1,4 +1,4 @@
-﻿using AtomicAssetsApiClient.Accounts;
+using AtomicAssetsApiClient.Accounts;
 using AtomicAssetsApiClient.Assets;
 using AtomicAssetsApiClient.Burns;
 using AtomicAssetsApiClient.Collections;
@@ -13,28 +13,36 @@ namespace AtomicAssetsApiClient
     public class AtomicAssetsApiFactory
     {
         private readonly string _baseUrl;
-        private const string Version1BaseUrl = "http://api.wax.liquidstudios.io/atomicassets/v1";
+        private const string AtomicAssetsIO = "https://wax.api.atomicassets.io/atomicassets/v1";
+        private const string EOSAmsterdam = "https://wax-aa.eu.eosamsterdam.net/atomicassets/v1";
+        private const string EOSAuthority = "https://aa-api-wax.eosauthority.com/atomicassets/v1";
+        private const string testnet3DK = "https://testatomic.3dkrender.com/atomicassets/v1";
+        private const string testnetNefty = "https://aa-testnet-public.neftyblocks.com/atomicassets/v1";
 
         private AtomicAssetsApiFactory(string baseUrl) => _baseUrl = baseUrl;
 
-        public static AtomicAssetsApiFactory Version1 => new AtomicAssetsApiFactory(Version1BaseUrl);
+        public static AtomicAssetsApiFactory MainNet1 => new AtomicAssetsApiFactory(AtomicAssetsIO);
+        public static AtomicAssetsApiFactory MainNet2 => new AtomicAssetsApiFactory(EOSAmsterdam);
+        public static AtomicAssetsApiFactory MainNet3 => new AtomicAssetsApiFactory(EOSAuthority);
+        public static AtomicAssetsApiFactory TestNet1 => new AtomicAssetsApiFactory(testnet3DK);
+        public static AtomicAssetsApiFactory TestNet2 => new AtomicAssetsApiFactory(testnetNefty);
 
-        public AccountsApi AccountsApi => new AccountsApi(_baseUrl);
+        public AccountsApi AccountsApi => new AccountsApi(_baseUrl, new HttpHandler());
 
-        public AssetsApi AssetsApi => new AssetsApi(_baseUrl);
-        
-        public BurnsApi BurnsApi => new BurnsApi(_baseUrl);
+        public AssetsApi AssetsApi => new AssetsApi(_baseUrl, new HttpHandler());
 
-        public CollectionsApi CollectionsApi => new CollectionsApi(_baseUrl);
-        
-        public ConfigApi ConfigApi => new ConfigApi(_baseUrl);
-        
-        public OffersApi OffersApi => new OffersApi(_baseUrl);
+        public BurnsApi BurnsApi => new BurnsApi(_baseUrl, new HttpHandler());
 
-        public SchemasApi SchemasApi => new SchemasApi(_baseUrl);
-        
-        public TemplatesApi TemplatesApi => new TemplatesApi(_baseUrl);
-        
-        public TransfersApi TransfersApi => new TransfersApi(_baseUrl);
+        public CollectionsApi CollectionsApi => new CollectionsApi(_baseUrl, new HttpHandler());
+
+        public ConfigApi ConfigApi => new ConfigApi(_baseUrl, new HttpHandler());
+
+        public OffersApi OffersApi => new OffersApi(_baseUrl, new HttpHandler());
+
+        public SchemasApi SchemasApi => new SchemasApi(_baseUrl, new HttpHandler());
+
+        public TemplatesApi TemplatesApi => new TemplatesApi(_baseUrl, new HttpHandler());
+
+        public TransfersApi TransfersApi => new TransfersApi(_baseUrl, new HttpHandler());
     }
 }
